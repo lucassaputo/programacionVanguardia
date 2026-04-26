d707ebb5d43380e5ed7af159d67c05bb30e2db60
Laura <lauranicolinoln3@gmail.com>
2026-04-26 18:00:50 -0300

    feat: agrego editor con sintaxis y historial

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-function analizar() {
+window.onload = function () {
+
+  const textarea = document.getElementById("codigo");
+  const boton = document.getElementById("btnAnalizar");
   const resultado = document.getElementById("resultado");
+  const historial = document.getElementById("historial");
 
-  // Simulación de respuesta
-  const tipo = Math.random();
+  const editor = CodeMirror.fromTextArea(textarea, {
+    lineNumbers: true,
+    mode: "javascript"
+  });
 
-  if (tipo < 0.33) {
-    resultado.innerText = "Error crítico: posible vulnerabilidad";
-    resultado.style.backgroundColor = "#ffdddd";
-    resultado.style.color = "#a70000";
-  } 
-  else if (tipo < 0.66) {
-    resultado.innerText = "Warning: se puede mejorar el código";
-    resultado.style.backgroundColor = "#fff3cd";
-    resultado.style.color = "#856404";
-  } 
-  else {
-    resultado.innerText = "Todo correcto ✔️";
-    resultado.style.backgroundColor = "#d4edda";
-    resultado.style.color = "#155724";
-  }
-   //Preparado para backend!!!
-  // Acá después se reemplaza esta simulación por un fetch al backend
-}
+  boton.onclick = function () {
+
+    const tipo = Math.random();
+    const item = document.createElement("li");
+
+    if (tipo < 0.33) {
+      resultado.innerText = "Error crítico: posible vulnerabilidad";
+      resultado.style.backgroundColor = "#ffdddd";
+      resultado.style.color = "#a70000";
+    } 
+    else if (tipo < 0.66) {
+      resultado.innerText = "Warning: se puede mejorar el código";
+      resultado.style.backgroundColor = "#fff3cd";
+      resultado.style.color = "#856404";
+    } 
+    else {
+      resultado.innerText = "Todo correcto ✔️";
+      resultado.style.backgroundColor = "#d4edda";
+      resultado.style.color = "#155724";
+    }
+
+    item.innerText = resultado.innerText;
+    historial.appendChild(item);
+
+  };
+
+};
